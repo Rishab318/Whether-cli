ef488929b1af56461876528f9c2d82c30c2f9988
Rishab318 <167055329+Rishab318@users.noreply.github.com>
2025-05-08 12:57:34 +0530

    fix: added new package related to semantic release

--- .releaserc.js
+++ .releaserc.js
@@ -10,16 +10,5 @@ module.exports = {
       },
     ],
     "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json"],
-        // Use a commit message that won't trigger another workflow run
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
   ],
-  // Explicitly set the repository URL including auth token
-  repositoryUrl: `https://\${process.env.GITHUB_TOKEN}@github.com/\${process.env.GITHUB_REPOSITORY}.git`,
 };
